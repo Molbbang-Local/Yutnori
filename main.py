bcace575f91ebe6670b6ac1694c97389b38ea35b
Jeong-Rae <kkwjdfo@gmail.com>
2023-11-04 00:37:56 +0900

    Feat: node index

--- main.py
+++ main.py
@@ -1,77 +1,82 @@
 import pygame
-import os
-
-class Field:
-    screen_width = 10800
-    screen_height = 720
-    node_image = pygame.image.load('resource/node.png')
-    node_size = node_image.get_size()
-    background_color = (255, 255, 255)
-    # 간격을 노드 크기의 75%로 설정
-    spacing_x = int(node_size[0] * 0.75)
-    spacing_y = int(node_size[1] * 0.75)
-    # base_x와 base_y를 새로운 간격에 맞게 조정
-    base_x = (screen_width - 21 * spacing_x) // 2
-    base_y = (screen_height - 21 * spacing_y) // 2
-    board_layout = [
-    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1]]
-
-    def fill_board(self, screen):
-        # 화면을 배경색으로 채우기
-        screen.fill(self.background_color)
-
-        # 윷놀이판 그리기
-        for y, row in enumerate(self.board_layout):
-            for x, cell in enumerate(row):
-                if cell == 1:
-                    # 이미지를 그릴 좌표 계산, 간격을 사용
-                    pos_x = self.base_x + x * self.spacing_x
-                    pos_y = self.base_y + y * self.spacing_y
-                    # 이미지 그리기
-                    screen.blit(self.node_image, (pos_x, pos_y))
+import sys
 
 # Pygame 초기화
 pygame.init()
 
-# 게임 화면 설정
-screen = pygame.display.set_mode((Field.screen_width, Field.screen_height))
+# 화면 설정
+screen_width = 1080
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-# 윷놀이판 객체 생성
-field = Field()
+# 배경 이미지 로드
+background = pygame.image.load('resource/fieldN.jpg')
+
+# node 변수 초기화
+node = None
+
+# 노드 값에 대한 좌표 범위를 저장하는 딕셔너리
+node_ranges = {
+    0: ((700, 725), (500, 530)),
+    1: ((700, 725), (430, 460)),
+    2: ((700, 725), (370, 400)),
+    3: ((700, 725), (310, 340)),
+    4: ((700, 725), (250, 280)),
+    5: ((700, 725), (180, 210)),
+    6: ((620, 650), (180, 210)),
+    7: ((560, 590), (180, 210)),
+    8: ((500, 530), (180, 210)),
+    9: ((440, 470), (180, 210)),
+    10: ((360, 390), (180, 210)),
+    11: ((360, 390), (250, 280)),
+    12: ((360, 390), (310, 340)),
+    13: ((360, 390), (370, 400)),
+    14: ((360, 390), (430, 460)),
+    15: ((360, 390), (500, 530)),
+    16: ((440, 470), (500, 530)),
+    17: ((500, 530), (500, 530)),
+    18: ((560, 590), (500, 530)),
+    19: ((620, 650), (500, 530)),
+    21: ((635, 665), (240, 270)),
+    22: ((430, 460), (240, 270)),
+    23: ((600, 630), (280, 310)),
+    24: ((470, 500), (280, 310)),
+    25: ((525, 555), (340, 370)),
+    26: ((600, 630), (400, 430)),
+    27: ((470, 500), (400, 430)),
+    28: ((635, 655), (445, 475)),
+    29: ((430, 455), (445, 475)),
+}
+
+def check_node_value(mouse_pos):
+    """
+    마우스 위치에 따라 node 값을 결정하는 함수
+    """
+    for node, ((x_start, x_end), (y_start, y_end)) in node_ranges.items():
+        if x_start <= mouse_pos[0] <= x_end and y_start <= mouse_pos[1] <= y_end:
+            return node
+    return None
 
 # 게임 루프
 running = True
 while running:
-    # 이벤트 처리
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # 마우스 버튼이 눌렸는지 확인
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # 함수를 사용하여 node 값 설정
+            new_node = check_node_value(event.pos)
+            if new_node is not None:
+                node = new_node
+                print(f"Node value changed to {node}")
 
-    # 윷놀이판 그리기
-    field.fill_board(screen)
+    # 배경 이미지를 화면에 그리기
+    screen.blit(background, (0, 0))
 
     # 화면 업데이트
-    pygame.display.flip()
+    pygame.display.update()
 
 # Pygame 종료
 pygame.quit()
+sys.exit()
